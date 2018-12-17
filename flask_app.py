b61581869c64fd2fba802f1d500e6fadd45f2d17
Xenofuscus <shabbirahadh@gmail.com>
2018-12-17 16:15:21 +0000

    Accounted for rounding and fixed title

--- flask_app.py
+++ flask_app.py
@@ -18,22 +18,22 @@ def process_inputs():
         flash("Grades must be a number!")
         return render_template("main_page.html", output="")
     # Check if they are valid grades:
-    firstQuart = int(sfirstQuart)
-    secondQuart = int(ssecondQuart)
-    targetFinal = int(stargetFinal)
-    if firstQuart > 100 or firstQuart < 0 or secondQuart > 100 or secondQuart < 0:
+    firstQuart = float(sfirstQuart)
+    secondQuart = float(ssecondQuart)
+    targetFinal = float(stargetFinal)
+    if firstQuart > 100.0 or firstQuart < 0 or secondQuart > 100.0 or secondQuart < 0:
         flash("Grades must be between 0-100!")
         return render_template("main_page.html", output="")
 
     # Do the stuff
-    finalGrade = calc_target_final_grade(firstQuart, secondQuart, targetFinal)
-    if finalGrade < 0:
+    finalGrade = int(round(calc_target_final_grade(firstQuart, secondQuart, targetFinal)))
+    if finalGrade <= 0:
         outString = "You could skip the final :)"
     elif finalGrade > 100:
         outString = "Oof! Choose a more realistic target grade :("
     else:
-        outString = "Aim for a " + str(finalGrade) + " on the final for a final grade of " + str(targetFinal) + " in the class."
+        outString = "Aim for a " + str(finalGrade) + " on the final for a final grade of " + str(int(targetFinal)) + " in the class."
     return render_template("main_page.html", output=outString)
 
 def calc_target_final_grade(first, second, final):
-    return (10 * final) - (4.5 * (first + second))
+    return (10 * (final - 0.5)) - (4.5 * (first + second))
